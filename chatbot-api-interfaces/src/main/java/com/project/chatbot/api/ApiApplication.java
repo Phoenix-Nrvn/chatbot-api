f09982879ed2c208e1cc82115d6ebbf1c765266e
Phoenix-Nrvn <2449584635@qq.com>
2023-03-08 20:20:25 +0800

    feat: init ddd project

---
+++ chatbot-api-interfaces/src/main/java/com/project/chatbot/api/ApiApplication.java
@@ -0,0 +1,18 @@
+package com.project.chatbot.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author LISHANSHAN
+ * @ClassName ApiApplication
+ * @Description 启动入口
+ * @date 2023/03/08
+ */
+@SpringBootApplication
+public class ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
---
+++ chatbot-api-interfaces/src/test/java/com/project/chatbot/api/ApiTest.java
@@ -0,0 +1,11 @@
+package com.project.chatbot.api;
+
+/**
+ * @author LISHANSHAN
+ * @ClassName ApiTest
+ * @Description 单元测试
+ * @date 2023/03/08
+ */
+
+public class ApiTest {
+}
